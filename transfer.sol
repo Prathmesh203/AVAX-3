0a4146bca175f571d3079ab050ec59fe00cca6fd
Prathmesh203 <117448429+Prathmesh203@users.noreply.github.com>
2024-01-05 22:47:08 +0530

    Update transfer.sol

--- transfer.sol
+++ transfer.sol
@@ -4,25 +4,24 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract mintToken is ERC20, Ownable {
+contract MintToken is ERC20, Ownable {
 
-constructor() ERC20("mintERCT", "ERC") Ownable(msg.sender) {
-    _mint(_msgSender(), 1000 * 10 ** decimals());
-}
+    constructor() ERC20("mintERCT", "ERC") Ownable(msg.sender) {
+        _mint(_msgSender(), 1000 * 10 ** decimals());
+    }
 
     // Function to mint new tokens
     function mint(address account, uint256 amount) external onlyOwner {
         _mint(account, amount);
     }
 
-    // Function to burn tokens
-    function burn(uint256 amount) external onlyOwner {
-    _burn(_msgSender(), amount);
-}
+    // Function to burn tokens without onlyOwner modifier
+    function burn(uint256 amount) external {
+        _burn(_msgSender(), amount);
+    }
 
     // Function to transfer tokens with an additional check
     function transferWithCheck(address to, uint256 amount) external {
-
         _transfer(_msgSender(), to, amount);
     }
 }
